ef1b3301d365ab8286c6a371d1f035b38a6ce410
jamiromic <jamiromic@gmail.com>
2022-08-25 17:49:52 +0200

    bonus1_done

---
+++ Bonus1/js/app.js
@@ -0,0 +1,45 @@
+const todos = [
+    {
+        text: 'Fare la Spesa',
+        done: false,
+    },
+    {
+        text: 'Andare in palestra',
+        done: true,
+    },
+    {
+        text: 'Guardare l\'inter in TV',
+        done: false,
+    },
+]
+
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        todos: todos,
+        classDefaultTodo: 'todo',
+        newToDo: '',
+    },
+    methods: {
+        toggleIsDone(todo) {
+            todo.done = !todo.done
+        },
+        addToDo(){
+            this.newToDo = this.newToDo.trim()
+
+            if (!this.newToDo) return
+
+            this.todos.push({
+                text: this.newToDo,
+                done: false,
+            })
+
+            this.newToDo = ''
+        }
+    },
+
+
+})
+
+
