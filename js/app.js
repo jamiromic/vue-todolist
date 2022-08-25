0de89916b939d7ea1662f96dcd2057b9afb10c66
jamiromic <jamiromic@gmail.com>
2022-08-25 14:04:59 +0200

    milestone1_done

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const todos = [
+    {
+        text: 'Fare la Spesa',
+        done: false,
+    },
+    {
+        text: 'Andare in palestra',
+        done: true,
+    },
+    {
+        text: 'Guardare l\'inter in TV',
+        done: false,
+    },
+]
+
+
+const app = new Vue({
+    el: '#root',
+    data: {
+        todos: todos,
+        classDefaultTodo: 'todo',
+    },
+
+
+
+})
+
